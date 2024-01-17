40b7aa8d4fbe4cd54d07fcba4b8259289bba057a
Halil Hodzic <habetech@gmail.com>
2024-01-17 17:11:30 +0100

    uo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,11 +16,6 @@
     @livewireStyles
 
 
-    <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
-<script src="{{ mix('js/app.js') }}" defer></script>
-
 </head><script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <body>
     <div id="app">
